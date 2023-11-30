c5df02a1eaa66d8c3e921d9eac217f76e0399713
vn542da <julio.campaz@walmart.com>
2023-11-30 11:29:15 -0500

    Chapter 5

--- src/main/java/_04_05/before/Main.java
+++ src/main/java/_04_05/before/Main.java
@@ -4,13 +4,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) {
 
         List<String> fruit = Arrays.asList("apple", "pineapple", "banana", "pear", "strawberry");
-        List<String> capitalisedFruit = processWithoutStreams(fruit);
+        List<String> capitalisedFruit = processWithStreams(fruit);
         System.out.println(capitalisedFruit);
     }
 
@@ -27,4 +28,12 @@ public class Main {
         return capitalisedFruit;
     }
 
+
+    static List<String> processWithStreams(List<String> fruit){
+        return fruit.stream()
+                .map(String::toUpperCase)
+                .filter(item -> item.startsWith("P"))
+                .sorted()
+                .collect(Collectors.toList());
+    }
 }
--- src/main/java/_05_02/before/Main.java
+++ src/main/java/_05_02/before/Main.java
@@ -3,7 +3,14 @@ package _05_02.before;
 public class Main {
 
     public static void main(String[] args) {
+        Thread thread1 = new ThreadExample();
+        Thread thread2 = new ThreadExample();
 
+        thread1.setName("First thread");
+        thread2.setName("Second thread");
+
+        thread1.start();
+        thread2.start();
     }
 
 }
