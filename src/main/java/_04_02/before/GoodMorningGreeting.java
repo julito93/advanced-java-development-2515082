b9cf60a70666cfa869a8dec551e6b462c731d318
vn542da <julio.campaz@walmart.com>
2023-11-30 10:01:56 -0500

    Chapter 4 exercise 2

--- src/main/java/_04_02/before/GoodMorningGreeting.java
+++
@@ -1,8 +0,0 @@
-package _04_02.before;
-
-public class GoodMorningGreeting implements Greeting {
-    @Override
-    public void printMessage() {
-        System.out.println("Good morning");
-    }
-}
--- src/main/java/_04_02/before/Main.java
+++ src/main/java/_04_02/before/Main.java
@@ -3,10 +3,13 @@ package _04_02.before;
 public class Main {
 
     public static void main(String[] args) {
-        HelloWorldGreeting helloWorldGreeting = new HelloWorldGreeting();
+        Greeting helloWorldGreeting = () -> System.out.println("HelloWorld");
         helloWorldGreeting.printMessage();
-        GoodMorningGreeting goodMorningGreeting = new GoodMorningGreeting();
+        Greeting goodMorningGreeting = () -> System.out.println("Good Morning");;
         goodMorningGreeting.printMessage();
+
+        Greeting goodAfternoonGreeting = () -> System.out.println("Good afternoon");
+        goodAfternoonGreeting.printMessage();
     }
 
 }
